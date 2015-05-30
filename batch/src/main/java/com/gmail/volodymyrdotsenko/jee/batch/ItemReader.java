c07f057533092adbd240ee57614d8e4c132ff7d4
volodymyrd <volodymyrdotsenko@gmail.com>
2015-05-30 22:29:10 +0300

    batch

---
+++ batch/src/main/java/com/gmail/volodymyrdotsenko/jee/batch/ItemReader.java
@@ -0,0 +1,12 @@
+package com.gmail.volodymyrdotsenko.jee.batch;
+
+import javax.batch.api.chunk.AbstractItemReader;
+
+public class ItemReader extends AbstractItemReader {
+
+	@Override
+	public Object readItem() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
